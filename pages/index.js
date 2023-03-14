6c43f1c1b974b9a0214e0d1a35c6c80aa14f9c4b
Alihaitham26 <alihaithamahmed26@gmail.com>
2023-03-14 12:15:59 +0200

    add new function to posts.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/Layout';
 import utilStyles from '../styles/utils.module.css';
-import getSortedPostsData from '../lib/posts';
+import { getSortedPostsData } from '../lib/posts';
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData()
